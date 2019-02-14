ad1bfb7489a367df26171f15fd6dbcb201e0c3cc
Isaac Merchan Blanco <imerchan@beta21.aulas.gsyc.urjc.es>
2019-02-14 10:43:01 +0100

    numeros en display

--- calculadorajs.js
+++ calculadorajs.js
@@ -6,13 +6,50 @@ function main() {
   boton1.onclick = () => {
     display.innerHTML = boton1.innerHTML
   }
+
   var boton2 = document.getElementById('boton2');
+  boton2.onclick = () => {
+    display.innerHTML = boton2.innerHTML
+  }
+
   var boton3 = document.getElementById('boton3');
+  boton3.onclick = () => {
+    display.innerHTML = boton3.innerHTML
+  }
+
   var boton4 = document.getElementById('boton4');
+  boton4.onclick = () => {
+    display.innerHTML = boton4.innerHTML
+  }
+
   var boton5 = document.getElementById('boton5');
+  boton5.onclick = () => {
+    display.innerHTML = boton5.innerHTML
+  }
+
   var boton6 = document.getElementById('boton6');
+  boton6.onclick = () => {
+    display.innerHTML = boton6.innerHTML
+  }
+
   var boton7 = document.getElementById('boton7');
+  boton7.onclick = () => {
+    display.innerHTML = boton7.innerHTML
+  }
+
   var boton8 = document.getElementById('boton8');
+  boton8.onclick = () => {
+    display.innerHTML = boton8.innerHTML
+  }
+
   var boton9 = document.getElementById('boton9');
+  boton9.onclick = () => {
+    display.innerHTML = boton9.innerHTML
+  }
+
   var boton0 = document.getElementById('boton0');
+  boton0.onclick = () => {
+    display.innerHTML = boton0.innerHTML
+  }
+
 }
