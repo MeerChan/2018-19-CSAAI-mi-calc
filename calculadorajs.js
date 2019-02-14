ad3fb6c622bbed57fc34e1ba16b6076972abf611
Isaac Merchan Blanco <imerchan@beta21.aulas.gsyc.urjc.es>
2019-02-14 10:27:00 +0100

    botones

--- calculadorajs.js
+++ calculadorajs.js
@@ -1,12 +1,18 @@
-function main() {
-  console.log("hola");
+funtion main() {
 
-  var boton = document.getElementById('boton');
+  var display = document.getElementById('display');
 
-  boton.onclick = () => {
-    console.log("click");
-
-    var display = document.getElementById('display');
-    display.innerHTML = " Nuevo texto";
+  var boton1 = document.getElementById('boton1');
+  boton1.onclick = () => {
+    display.innerHTML = boton1;
   }
+  var boton2 = document.getElementById('boton2');
+  var boton3 = document.getElementById('boton3');
+  var boton4 = document.getElementById('boton4');
+  var boton5 = document.getElementById('boton5');
+  var boton6 = document.getElementById('boton6');
+  var boton7 = document.getElementById('boton7');
+  var boton8 = document.getElementById('boton8');
+  var boton9 = document.getElementById('boton9');
+  var boton0 = document.getElementById('boton0');
 }
